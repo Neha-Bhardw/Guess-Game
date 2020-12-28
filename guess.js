9ede6b935836a3d6c32088d04a5385e274d9d6a9
Neha <76679712+Neha-Bhardw@users.noreply.github.com>
2020-12-28 17:39:41 +0530

    Add files via upload
    In this Demo
    This is a activity that depicts the use of Loops like while, if and else. In this the user writes a number and then a random number is generated later the user has to guess what number it might be. It also shows as to how many attempts the user has made to guess the number.

---
+++ guess.js
@@ -0,0 +1,29 @@
+let maximum = parseInt(prompt('Enter the Maximum Number!'));
+while (!maximum) {
+    maximum = parseInt(prompt('Enter a valid number!'));
+}
+const targetNum = Math.floor(Math.random() * maximum + 1);
+console.log(targetNum)
+
+let guess = parseInt(prompt('Enter your first guess!'));
+let attempts = 1;
+
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attempts++;
+    if (guess > targetNum) {
+        guess = prompt('Too High! Enter a new guess:');
+    } else {
+        guess = prompt('Too Low! Enter a new guess:');
+    }
+}
+
+if (guess === 'q') {
+    console.log('OK, YOU QUIT!');
+    console.log(`You made ${attempts} guesses`);
+} else {
+    console.log('Congrats! YOU WIN');
+    console.log(`You Got it! It took you ${attempts} guesses`);
+}
+
+
